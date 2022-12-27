f8f5331fc32516de5016f3421dcf8925b31ffdef
dinesh <dineshrock153@gmail.com>
2022-12-27 19:08:32 +0530

    check4

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -29,32 +29,32 @@ public class F1 extends Car {
 
         else if(newSpeed > 250) {
             changeGear(6);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
 
         else if(newSpeed > 200){
             changeGear(5);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
 
         else if(newSpeed > 150){
 
             changeGear(4);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
 
         else if(newSpeed > 100){
             changeGear(3);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
 
         else if(newSpeed > 50){
             changeGear(2);
-            changeSpeed(newSpeed, getCurrentDirection());
+
         }
 
         else if(newSpeed > 0){
-            changeGear(1);
+
             changeSpeed(newSpeed, getCurrentDirection());
         }
 
